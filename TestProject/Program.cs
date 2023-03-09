45060f834bf0eec3d45a91c7765f7bb96739c922
iranzivenant88 <46546731+iranzivenant88@users.noreply.github.com>
2023-03-09 13:01:28 -0700

    changes

---
+++ TestProject/Program.cs
@@ -0,0 +1,47 @@
+﻿//
+
+Random dice = new Random();
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
+//int roll1 = 6;
+//int roll2 = 6;
+//int roll3 = 6;
+int total = roll1 + roll2 + roll3;
+
+Console.WriteLine($"Dice rolls: {roll1 } + {roll2 } + { roll3} ={total}");
+
+if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+{
+    if ((roll1 == roll2) && (roll2 == roll3))
+    {
+        Console.WriteLine("You rolled triples! +6 bonus to tatal!");
+        total += 6;
+    }
+    else
+    {
+        Console.WriteLine("You rolled double! +2 bonus to tatal!");
+        total += 2;
+    }
+}
+
+
+
+if (total >= 16)
+{
+    Console.WriteLine("you win a car !");
+}
+else if (total >= 10)
+{
+    Console.WriteLine("you win a laptop ");
+}
+else if (total == 7)
+{
+    Console.WriteLine("you win a trip for two!");
+
+}
+else
+{
+    Console.WriteLine("You win a Kitten");
+}
+
