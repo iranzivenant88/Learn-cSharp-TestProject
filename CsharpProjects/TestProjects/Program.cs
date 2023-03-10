6f5d496be471f8e86d782424f2a490e238c56558
iranzivenant88 <46546731+iranzivenant88@users.noreply.github.com>
2023-03-10 13:51:49 -0700

    commit-3

--- CsharpProjects/TestProjects/Program.cs
+++ CsharpProjects/TestProjects/Program.cs
@@ -12,3 +12,13 @@ flaudulentOrderIDs[0] ="F000";
 Console.WriteLine($"Reassign First: {flaudulentOrderIDs[0]}");
 Console.WriteLine($"There are {flaudulentOrderIDs.Length} flaudulent orders to process.");
 
+int[]inventory = {200,450,700,175,250};
+int sum = 0;
+int bin = 0;
+foreach(int items in inventory){
+    sum+=items;
+    bin++;
+     Console.WriteLine($" Bin {bin} = {items } items (Running total : { sum } )");
+}
+ Console.WriteLine($" We have  {sum } items in inventory. )");
+
