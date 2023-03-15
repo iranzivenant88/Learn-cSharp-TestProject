2e91c614c46d2836261b82e332e3652467346f6d
iranzivenant88 <46546731+iranzivenant88@users.noreply.github.com>
2023-03-15 12:35:40 -0600

    switch

--- CsharpProjects/TestProjects/Program.cs
+++ CsharpProjects/TestProjects/Program.cs
@@ -1,24 +1,51 @@
-﻿Console.Write("Enter a number: ");
-double num1 =Convert.ToDouble(Console.ReadLine());
+﻿
+        //  Case 1: 
+        //      dayName = "Sunday";
+        //      break;
+        //  Case 2: 
+        //      dayName = "Sunday";
+        //      break;
+        //  Case 3: 
+        //      dayName = "Sunday";
+        //      break;
 
-Console.Write("Enter Operator: ");
-string op = Console.ReadLine();
+        //  Case 0: 
+        //      dayName = "Sunday";
+        //      break;
+             
+        //  Case 0: 
+        //      dayName = "Sunday";
+        //      break;
+        //  Case 0: 
+        //      dayName = "Sunday";
+        //      break;
+   // }
 
-Console.Write("Enter a number: ");
-double num2 =Convert.ToDouble(Console.ReadLine());
+ 
 
-if(op=="+"){
-    Console.WriteLine(num1+num2);
 
-}else if(op =="-"){
-     Console.WriteLine(num1-num2);
 
-}else if(op=="*"){
-    Console.WriteLine(num1 * num2);
+// Console.Write("Enter a number: ");
+// double num1 =Convert.ToDouble(Console.ReadLine());
 
-}else if(op =="/"){
-     Console.WriteLine(num1 / num2);
+// Console.Write("Enter Operator: ");
+// string op = Console.ReadLine();
 
-}else{
-    Console.WriteLine("Invalid Operator");
-}
+// Console.Write("Enter a number: ");
+// double num2 =Convert.ToDouble(Console.ReadLine());
+
+// if(op=="+"){
+//     Console.WriteLine(num1+num2);
+
+// }else if(op =="-"){
+//      Console.WriteLine(num1-num2);
+
+// }else if(op=="*"){
+//     Console.WriteLine(num1 * num2);
+
+// }else if(op =="/"){
+//      Console.WriteLine(num1 / num2);
+
+// }else{
+//     Console.WriteLine("Invalid Operator");
+// }
--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,28 +1,70 @@
-﻿Random dice = new Random();
-int roll1 = dice.Next();
-int roll2 = dice.Next(101);
-int roll3 = dice.Next(50,101);
-Console.WriteLine($"First roll: {roll1}");
-Console.WriteLine($"First roll: {roll2}");
-Console.WriteLine($"First roll: {roll3}");
+﻿using System.Text;
+using System.Threading.Tasks;
+namespace Giraffe{
+    class Program
+    {
+        static void Main(string[]args){
+            Console.WriteLine(GetDay(4));
+
+            Console.ReadLine();
+        }
+        static string GetDay(int dayNum){
+            string dayName;
+            switch(dayNum){
+                case 0: 
+                     dayName = "Sunday";
+                     break;
+            }
+              switch(dayNum){
+                case 1: 
+                     dayName = "Sunday";
+                     break;
+            }
+              switch(dayNum){
+                case 2: 
+                     dayName = "Sunday";
+                     break;
+            }
+              switch(dayNum){
+                case 3: 
+                     dayName = "Sunday";
+                     break;
+            }
+              switch(dayNum){
+                case 4: 
+                     dayName = "Sunday";
+                     break;
+            }
+              switch(dayNum){
+                case 5: 
+                     dayName = "Sunday";
+                     break;
+            }
+              switch(dayNum){
+                case 6: 
+                     dayName = "Sunday";
+                     break;
+                default:
+                     dayName= "Invalid day number";
+              break;
+            }
+             
 
 
+            return dayName;
+            
+        }
+        
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    }
+}
+// Random dice = new Random();
+// int roll1 = dice.Next();
+// int roll2 = dice.Next(101);
+// int roll3 = dice.Next(50,101);
+// Console.WriteLine($"First roll: {roll1}");
+// Console.WriteLine($"First roll: {roll2}");
+// Console.WriteLine($"First roll: {roll3}");
 
 
 
